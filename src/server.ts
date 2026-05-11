e0cb394a4246aae9911a705e8c6ae53cbf1b57c8
Kuray Karaaslan <kuraykaraaslan@gmail.com>
2026-05-11 13:56:41 +0300

    apply theme

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3003;
 
 app.listen(PORT, () => {
   console.log(`02_EJS_Components → http://localhost:${PORT}`);
